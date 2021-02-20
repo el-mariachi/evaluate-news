ca2e8a0400fc31f1cf978c2234d456801fc46ce2
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-02-20 12:40:54 +0300

    Install basic dependencies. Fix version conflicts

--- webpack.dev.js
+++ webpack.dev.js
@@ -1,7 +1,7 @@
-const path = require('path')
-const webpack = require('webpack')
-const HtmlWebPackPlugin = require("html-webpack-plugin")
-const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+// const path = require('path')
+// const webpack = require('webpack')
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
@@ -11,7 +11,7 @@ module.exports = {
     module: {
         rules: [
             {
-                test: '/\.js$/',
+                test: /\.js$/,
                 exclude: /node_modules/,
                 loader: "babel-loader"
             }
