a4e6d0f9ed0351b21d4ce13c9de937b69c94d3cb
Dmitri Sokolov <mitson.mbox@gmail.com>
2021-02-22 17:55:21 +0300

    refactor: Move handleSelect to a separate file

--- src/client/index.js
+++ src/client/index.js
@@ -1,5 +1,6 @@
 import { checkForName } from './js/nameChecker'
 import { handleSubmit } from './js/formHandler'
+import { handleSelect } from './js/handleSelect';
 
 // import './styles/vars.scss';
 import './styles/resets.scss';
@@ -8,27 +9,10 @@ import './styles/footer.scss';
 import './styles/form.scss';
 import './styles/header.scss';
 
-/**
- * Input type selector handler
- */
-function handleSelect(event) {
-    const form = document.forms[0];
-    switch (this.selectedIndex) {
-        case 0:
-            form.classList.remove('form--input-text');
-            form.classList.add('form--input-url');
-            break;
-        case 1:
-            form.classList.add('form--input-text');
-            form.classList.remove('form--input-url');
-        default:
-            break;
-    }
-};
-
 document.getElementById('select').addEventListener('change', handleSelect);
 
 export {
     checkForName,
-    handleSubmit
+    handleSubmit,
+    handleSelect
 };
---
+++ src/client/js/handleSelect.js
@@ -0,0 +1,18 @@
+/**
+ * Input type selector handler
+ */
+export function handleSelect() {
+    const form = document.forms[0];
+    switch (this.selectedIndex) {
+        case 0:
+            form.classList.remove('form--input-text');
+            form.classList.add('form--input-url');
+            break;
+        case 1:
+            form.classList.add('form--input-text');
+            form.classList.remove('form--input-url');
+        default:
+            break;
+    }
+}
+;
